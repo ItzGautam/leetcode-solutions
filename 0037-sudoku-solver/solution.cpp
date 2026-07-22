bdf070778dbb8580eab13017700cddf87da1e9cc
Gautam <135647348+ItzGautam@users.noreply.github.com>
2026-07-22 10:33:08 +0000

    [LeetCode Sync] Runtime - 232 ms (79.89%), Memory - 8.8 MB (84.47%)

---
+++ 0037-sudoku-solver/solution.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+    bool isSafe(vector<vector<char>>& board,int row,int col,char n) {
+        for(int i = 0; i<9; i++) {
+            if(board[row][i]==n) {
+                return false;
+            }
+        }
+
+        for(int i = 0; i<9; i++) {
+            if(board[i][col]==n) {
+                return false;
+            }
+        }
+
+        int strow = (row/3)*3;
+        int stcol = (col/3)*3;
+
+        for(int i = strow; i<=strow+2 ; i++) {
+            for(int j = stcol; j<=stcol+2; j++) {
+                if(board[i][j]==n) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+    bool helper(vector<vector<char>>& board,int row, int col) {
+
+        if(row == 9) {
+            return true;
+        }
+
+        int nextrow = row;
+        int nextcol = col+1;
+
+        if(nextcol==9) {
+            nextrow = row+1;
+            nextcol = 0;
+        }
+
+        if(board[row][col]!='.') {
+            return  helper(board,nextrow,nextcol);
+        } 
+
+        for(int dig = '1'; dig<='9'; dig++) {
+            if(isSafe(board,row,col,dig)) {
+                board[row][col] = dig;
+                if(helper(board,nextrow,nextcol)) {
+                    return true;
+                }
+                board[row][col] = '.';            }
+            }
+
+            return false;
+        }
+
+
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board,0,0);       
+    }
+};
