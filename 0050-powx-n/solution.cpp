344f1dc21f6c3f626e30bb2e5ea054f830bdc488
Gautam <135647348+ItzGautam@users.noreply.github.com>
2026-09-09 05:56:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (44.61%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     double myPow(double x, int n) {
 
-        double ans =1;
-        long BinForm = n;
+        long long N = n;
 
-        if (BinForm < 0) {
-            x = 1/x;
-            BinForm = -BinForm;
-         }
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
 
-         while (BinForm >0) {
-            if (BinForm%2 == 1 ) {
+        double ans = 1;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
                 ans *= x;
             }
+
             x *= x;
-            BinForm /= 2;
-         }
-      return ans;
+            N /= 2;
+        }
+
+        return ans;
     }
 };
