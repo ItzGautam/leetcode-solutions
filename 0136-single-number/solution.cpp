993a028d709fca29aa355015dbf013c8ff88bcf8
Gautam <135647348+ItzGautam@users.noreply.github.com>
2026-09-11 10:53:14 +0000

    [LeetCode Sync] Runtime - 9 ms (15.01%), Memory - 24.7 MB (7.37%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for (int value:nums) {
-            ans^=value;
+
+        unordered_map<int,int>m;
+
+        stack<int> s;
+
+        for(int i =0; i<nums.size(); i++) {
+
+            m[nums[i]]++;
+
+            if (m[nums[i]] == 1) {
+                s.push(nums[i]);
+            }
         }
-        return ans;
-    }  
+
+        while(!s.empty()) {
+
+            if(m[s.top()] == 1) {
+                break;
+            }
+
+            s.pop();
+        }
+
+        return s.top();
+    }
 };
